7146e33e30385b2c818ca73b62875355eef2694c
Mrabbit22 <l.diaz.castrillo@gmail.com>
2023-11-15 15:09:40 +0100

    Aprendiendo a usar GitHub

--- src/main/java/com/example/practicap2p/Object195Controller.java
+++ src/main/java/com/example/practicap2p/Object195Controller.java
@@ -3,6 +3,7 @@ package com.example.practicap2p;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 
 public class Object195Controller {
@@ -18,12 +19,14 @@ public class Object195Controller {
 
     @FXML
     void addTab(ActionEvent event) {
-        //Añadi linea
+        Tab aux = new Tab();
+        aux.setText("Conexión");
+        this.TABPANE.getTabs().add(aux);
     }
 
     @FXML
     void removeTab(ActionEvent event) {
-
+        this.TABPANE.getTabs().remove(this.TABPANE.getTabs().size()-1);
     }
 
 }
